3b1122e5e8c0ad0e05c9554d0f1450572d2bb150
RoberP1 <roberpapeschi@gmail.com>
2021-12-21 20:15:21 -0300

    gamplay terminado

--- Test-Final-Prog/Assets/DEVELOPMENT/Scripts/MainMenu.cs
+++ Test-Final-Prog/Assets/DEVELOPMENT/Scripts/MainMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Slider Vol;
     public void Jugarbtn()
     {
         SceneManager.LoadScene("Game");
@@ -12,5 +14,11 @@ public class MainMenu : MonoBehaviour
     public void Salirbtn()
     {
         Application.Quit();
+    }
+    public void ChangeVolume()
+    {
+        AudioListener.volume = Vol.value;
+        PlayerPrefs.SetFloat("vol", Vol.value);
+
     }
 }
